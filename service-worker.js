89473b7ed87621e143ad51693aebca65a676bccf
Wilkor <wilkorguitarrista@msn.com>
2023-06-09 11:29:25 -0300

    fix
    Removendo url do google do projeto!

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3b16b2693ebe3b96241adccced5ae14b.js"
+  "./precache-manifest.ea413e491d4b13c9426a707a357353b6.js"
 );
 
 self.addEventListener('message', (event) => {
